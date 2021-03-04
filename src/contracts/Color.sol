5fcb0ed0170fbe039bf9e8801fcad138560299d6
Elliot Plant <elliotaplant@gmail.com>
2021-03-03 19:03:21 -0800

    Get ints working it

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -3,60 +3,14 @@ pragma solidity 0.5.0;
 import "./ERC721Full.sol";
 
 contract Color is ERC721Full {
-  string[] public colors;
-  mapping(string => uint) _colorToId;
-
   constructor() ERC721Full("Color", "COLOR") public {
   }
 
-  // E.G. color = "#FFFFFF"
-  function mint(string memory _color) public {
-    require(_colorToId[_color] == 0, "Color already exists");
-    bytes memory _colorBts = bytes(_color);
-    require(_colorBts.length == 7, "New colors must have 7 characters");
-    require(_colorBts[0] == "#", "New colors must start with a # character");
-    for (uint i = 1; i < 7; i += 2) {
-      require(_colorBts[i] >= "0" && _colorBts[i] <= "F", "New colors must use valid Hex digits");
-      require(_colorBts[i] == _colorBts[i+1], "New colors must have the format #AABBCC");
-    }
-
-    require(_colorBts[0] == "#");
-    uint _id = colors.push(_color);
-    _mint(msg.sender, _id);
-    _colorToId[_color] = _id;
+  // E.G. color = 16777215 == "#FFFFFF"
+  function mint(uint32 _color) public {
+    require(!_exists(_color), "Color already exists");
+    require(_color >= 0 && _color <= 16777215, "Color must be between 0 and 16777215 (#FFFFFF)");
+    require(_color % 17 == 0, "Color % 17 must be zero");
+    _mint(msg.sender, _color);
   }
-
-  // E.G. color = "#FFFFFF"
-  function blend(string memory _color1, string memory _color2) public {
-    // make sure caller owns both tokens
-    uint _id1 = _colorToId[_color1];
-    uint _id2 = _colorToId[_color2];
-    require(ownerOf(_id1) == msg.sender && ownerOf(_id2) == msg.sender, "You must own both colors to blend them");
-
-    // calculate new color
-    string memory _newColor = innerBlend(_color1, _color2);
-
-    // check if new color exists
-    require(_colorToId[_newColor] == 0, "Color already exists");
-
-    // create new token and give it to caller
-    uint _id = colors.push(_newColor);
-    _mint(msg.sender, _id);
-    _colorToId[_newColor] = _id;
-  }
-
-  function innerBlend(string memory _color1, string memory _color2) private returns (string memory color) {
-    // Convert strings to bytes for index access
-    bytes memory _colorBts1 = bytes(_color1);
-    bytes memory _colorBts2 = bytes(_color2);
-
-    bytes memory _newColor = bytes("#000000");
-
-    for (uint i = 1; i < 7; i += 2) {
-      _newColor[i] = (_colorBts1[i] + _colorBts1[i + 1]) / 2;
-    }
-
-    return string(_newColor);
-  }
-
 }
--- test/Color.test.js
+++ test/Color.test.js
@@ -29,19 +29,18 @@ contract('Color', (accounts) => {
       const symbol = await contract.symbol()
       assert.equal(symbol, 'COLOR')
     })
-
   })
 
   describe('minting', async () => {
 
     it('creates a new token', async () => {
-      const expected = '#EEBBDD'
+      const expected = hexToInt('#EEBBDD')
       const result = await contract.mint(expected)
       const totalSupply = await contract.totalSupply()
       // SUCCESS
       assert.equal(totalSupply, 1)
       const event = result.logs[0].args
-      assert.equal(event.tokenId.toNumber(), 1, 'id is correct')
+      assert.equal(event.tokenId.toNumber(), expected, 'id is correct')
       assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is correct')
       assert.equal(event.to, accounts[0], 'to is correct')
 
@@ -51,57 +50,67 @@ contract('Color', (accounts) => {
 
     it('only mints valid hex colors', async () => {
       await contract.mint('not a color').should.be.rejected; // must be the right length
-      await contract.mint('#AABBCCDD').should.be.rejected; // must be the right length
-      await contract.mint('#-11111').should.be.rejected; // no negative numbers
-      await contract.mint('#CLOSEB').should.be.rejected; // only valid letters allowed
-      await contract.mint('#ABCDEG').should.be.rejected; // only valid letters allowed
-      await contract.mint('#123abc').should.be.rejected; // uppercase only
-      await contract.mint('#112234').should.be.rejected; // new colors must have AABBCC format
+      await contract.mint(hexToInt('#AABBCCDD')).should.be.rejected; // must be the right length
+      await contract.mint(hexToInt('#-11111')).should.be.rejected; // no negative numbers
+      await contract.mint(hexToInt('#CLOSEB')).should.be.rejected; // only valid letters allowed
+      await contract.mint(hexToInt('#ABCDEG')).should.be.rejected; // only valid letters allowed
+      await contract.mint(hexToInt('#123abc')).should.be.rejected; // uppercase only
+      await contract.mint(hexToInt('#112234')).should.be.rejected; // new colors must have AABBCC format
     })
   })
 
   describe('indexing', async () => {
     it('lists colors', async () => {
       const colors = ['#AABBCC', '#FFFFFF', '#000000'];
+      const intColors = colors.map(hexToInt)
       // Mint 3 more tokens
-      for (const color of colors) {
+      for (const color of intColors) {
         await contract.mint(color);
       }
 
-      const totalSupply = await contract.totalSupply()
+      const tokenCount = await contract.balanceOf(accounts[0])
 
       let result = [];
 
-      for (var i = totalSupply - colors.length; i < totalSupply; i++) {
-        const color = await contract.colors(i);
-        result.push(color)
+      for (var i = tokenCount - colors.length; i < tokenCount; i++) {
+        const colorInt = await contract.tokenOfOwnerByIndex(accounts[0], i);
+        result.push(colorInt)
       }
 
-      expect(result).to.deep.equal(colors);
+      const hexResult = result.map(intToHex);
+      expect(hexResult).to.deep.equal(colors);
     })
   })
 
   describe('blending', async () => {
     it('blends colors', async () => {
-      const color1 = '#001122';
-      const color2 = '#DD99BB';
-      const expected = '#6F556F';
-      // Mint 3 more tokens
-      await contract.mint(color1);
-      await contract.mint(color2);
-
-      await contract.blend(color1, color2);
-
-      const totalSupply = await contract.totalSupply()
-
-      let result = [];
-
-      for (var i = totalSupply - colors.length; i < totalSupply; i++) {
-        const color = await contract.colors(i);
-        result.push(color)
-      }
-
-      expect(result).to.deep.equal([color1, color2, expected]);
+      // const color1 = '#001122';
+      // const color2 = '#DD99BB';
+      // const expected = '#6F556F';
+      // // Mint 3 more tokens
+      // await contract.mint(color1);
+      // await contract.mint(color2);
+      //
+      // await contract.blend(color1, color2);
+      //
+      // const totalSupply = await contract.totalSupply()
+      //
+      // let result = [];
+      //
+      // for (var i = totalSupply - colors.length; i < totalSupply; i++) {
+      //   const color = await contract.colors(i);
+      //   result.push(color)
+      // }
+      //
+      // expect(result).to.deep.equal([color1, color2, expected]);
     })
   })
 })
+
+function hexToInt(hex) {
+  return parseInt(hex.slice(1), 16);
+}
+
+function intToHex(int) {
+  return '#' + int.toString(16).padStart(6, '0').toUpperCase();
+}
